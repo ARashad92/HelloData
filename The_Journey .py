98f51326e2ac4e7360774fb0df1cea1480e20b07
Ahmed Rashad <ahmed.rashad7792@gmail.com>
2021-07-28 23:08:59 +0200

    Delete The_Journey .py

--- The_Journey .py
+++
@@ -1,9 +0,0 @@
-#H1 My Jupyter Notebook on IBM Watson Studio
-#Bolde Ahmed Rashad Adly 
-#italic I'm intersted in Data science because it's the future career and I love analysis and predection 
-#H3 The below code is assigning two integer variable and sum them
-var1 = 5 
-var2 = 4
-sumetion = var1 + var2
-print(sumetion)
-#Note I'm not able to work in Watson because of region restrication options allow only to use it as free only in London and FrankFurt and Egypt Cairo is not available 
